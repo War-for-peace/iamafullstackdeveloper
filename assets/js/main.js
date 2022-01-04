ab92ba46e6890b29ba056639ef39064229183b6b
warforpeace <43673957+warforpeace@users.noreply.github.com>
2022-01-04 14:15:33 +0530

    initial

--- assets/js/main.js
+++ assets/js/main.js
@@ -110,10 +110,10 @@ const sections = document.querySelectorAll("section[id]");
 function scrollActive() {
   const scrollY = window.pageYOffset;
 
-  sections.forEach(current => {
+  sections.forEach((current) => {
     const sectionHeight = current.offsetHeight;
     const sectionTop = current.offsetTop - 50;
-    sectionId = current.getAttribute('id');
+    sectionId = current.getAttribute("id");
 
     if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
       document
@@ -126,44 +126,107 @@ function scrollActive() {
     }
   });
 }
-window.addEventListener('scroll', scrollActive)
+window.addEventListener("scroll", scrollActive);
 
 /*==================== CHANGE BACKGROUND HEADER ====================*/
-function scrollHeader(){
-    const nav = document.getElementById('header')
+function scrollHeader() {
+  const nav = document.getElementById("header");
 
-    if (this.scrollY >= 80) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
+  if (this.scrollY >= 80) nav.classList.add("scroll-header");
+  else nav.classList.remove("scroll-header");
 }
-window.addEventListener('scroll', scrollHeader)
+window.addEventListener("scroll", scrollHeader);
 
 /*==================== SHOW SCROLL UP ====================*/
-function scrollUp(){
-    const scrollUp = document.getElementById('scrollup');
+function scrollUp() {
+  const scrollUp = document.getElementById("scrollup");
 
-    if (this.scrollY >= 560) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+  if (this.scrollY >= 560) scrollUp.classList.add("show-scroll");
+  else scrollUp.classList.remove("show-scroll");
 }
-window.addEventListener('scroll', scrollUp)
+window.addEventListener("scroll", scrollUp);
 
 /*==================== DARK LIGHT THEME ====================*/
-const themeButton = document.getElementById('theme-button')
-const darkTheme = 'dark-theme'
-const iconTheme = 'uil-sun'
+const themeButton = document.getElementById("theme-button");
+const darkTheme = "dark-theme";
+const iconTheme = "uil-sun";
 
-const selectedTheme = localStorage.getItem('selected-theme')
-const selectedIcon = localStorage.getItem('selected-icon')
+const selectedTheme = localStorage.getItem("selected-theme");
+const selectedIcon = localStorage.getItem("selected-icon");
 
-const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
-const getCurrentIcon = () => document.body.classList.contains(darkTheme) ? 'uil-moon' : 'uil-sun'
+const getCurrentTheme = () =>
+  document.body.classList.contains(darkTheme) ? "dark" : "light";
+const getCurrentIcon = () =>
+  document.body.classList.contains(darkTheme) ? "uil-moon" : "uil-sun";
 
-if(selectedTheme){
-    document.body.classList[selectedTheme==='dark'?'add':'remove'](darkTheme)
-    themeButton.classList[selectedTheme==='bx-moon'?'add':'remove'](iconTheme)
+if (selectedTheme) {
+  document.body.classList[selectedTheme === "dark" ? "add" : "remove"](
+    darkTheme
+  );
+  themeButton.classList[selectedTheme === "bx-moon" ? "add" : "remove"](
+    iconTheme
+  );
 }
 
-themeButton.addEventListener('click',()=>{
-    document.body.classList.toggle(darkTheme)
-    themeButton.classList.toggle(iconTheme)
+themeButton.addEventListener("click", () => {
+  document.body.classList.toggle(darkTheme);
+  themeButton.classList.toggle(iconTheme);
 
-    localStorage.setItem('selected-theme', getCurrentTheme())
-    localStorage.setItem('selected-icon', getCurrentIcon())
-})
+  localStorage.setItem("selected-theme", getCurrentTheme());
+  localStorage.setItem("selected-icon", getCurrentIcon());
+});
+
+/*==================== SAVING INPUT ====================*/
+
+let canSend = true;
+
+function save() {
+  if (canSend == true) {
+    canSend = false;
+
+    let name = document.getElementById("name").value;
+    let email = document.getElementById("email").value;
+    let title = document.getElementById("title").value;
+    let text = document.getElementById("text").value;
+
+    const sendM = document.getElementById("send-button").innerHTML;
+    document.getElementById(
+      "send-button"
+    ).innerHTML = `Sent <i class="uil uil-check button__icon"></i>`;
+    setTimeout(() => {
+      document.getElementById("send-button").innerHTML = `${sendM}`;
+      canSend = true;
+    }, 2500);
+
+    updatingFile(name, email, title, text);
+
+    document.getElementById("name").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("title").value = "";
+    document.getElementById("text").value = "";
+  } else {
+    console.log("already uploading");
+  }
+}
+
+var config = {
+  apiKey: "AIzaSyDIsyuPWzqi7UuSNewF6cST6IRNlQ-ARx0",
+  authDomain: "iamafullstackdeveloper.com",
+  databaseURL: "https://my-new-project-4e17a.firebaseio.com",
+  projectId: "my-new-project-4e17a",
+  storageBucket: "my-new-project-4e17a.appspot.com",
+  messagingSenderId: "277809443589",
+};
+firebase.initializeApp(config);
+
+var messagesRef = firebase.database().ref("messages");
+
+function updatingFile(name, email, title, text) {
+  var newMessageRef = messagesRef.push();
+  newMessageRef.set({
+    name: name,
+    title: title,
+    email: email,
+    text: text,
+  });
+}
